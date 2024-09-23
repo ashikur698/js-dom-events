1e0e22d72b977b5561449d7c3a6291aa2614b8e7
ashikur698 <ashikur698@.com>
2024-09-23 19:43:54 +0600

    added onclick

--- js/events.js
+++ js/events.js
@@ -1,1 +1,25 @@
-console.log('This is a special event different JS File')
+// Option 1: directly on html file
+
+
+// Option 2: Add onclick function
+function makeGreen(){
+    document.body.style.backgroundColor='lightgreen';
+}
+
+// Option 3: 
+const makeBlueButton = document.getElementById('make-blue')
+        makeBlueButton.onclick = makeBlue;
+
+        function makeBlue(){
+            document.body.style.backgroundColor = 'lightblue'
+        }
+
+// Option 3: 
+const purpleButton = document.getElementById('make-purple');
+        purpleButton.onclick = function makePurple(){
+            document.body.style.backgroundColor = 'plum'
+        }
+
+// function makeBlue(){
+//     document.body.style.backgroundColor='lightblue';
+// }
